47a76df3ee529fe98bbe8f27287e594f5b69056c
rohit.singh <singh.rohit19@outlook.com>
2023-01-26 22:11:28 +0530

    footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('slidein', [
-      transition(':enter', [
-        // when ngif has true
-        style({ transform: 'translateX(-100%)' }),
-        animate(250, style({ transform: 'translateX(0)' }))
-      ]),
-      transition(':leave', [
-        // when ngIf has false
-        animate(250, style({ transform: 'translateX(-100%)' }))
-      ])
-    ])
-  ]
 })
 export class AppComponent implements OnInit {
   title = 'Addyy';
@@ -25,12 +11,9 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  expanded: boolean = false;
 
   constructor() {
   }
 
-  toggleSideNav () {
-    
-  }
+
 }
